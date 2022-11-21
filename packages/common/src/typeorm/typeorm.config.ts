e42e86bbd5d353dd40a06d8481673bffca46d98d
limkeunhyeok <gorloom6425@naver.com>
2022-11-21 16:50:53 +0900

    fix: typeorm module

---
+++ packages/common/src/typeorm/typeorm.config.ts
@@ -0,0 +1,22 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { EntitySchema } from "typeorm";
+
+export const getTypeormConfig = (
+  entities: EntitySchema[]
+): TypeOrmModuleOptions => {
+  return {
+    type: "mysql",
+    host: process.env.DB_HOST || "127.0.0.1",
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "password",
+    port: Number(process.env.DB_PORT) || 3306,
+    database: process.env.DB_DATABASE || "develop",
+    timezone: "Z",
+    charset: "utf8_unicode_ci",
+    synchronize: Boolean(process.env.DB_SYNC) || true,
+    logging: Boolean(process.env.DB_LOGGING) || true, // 로그 레벨로도 가능
+    entities: entities,
+    migrations: [],
+    subscribers: [],
+  };
+};
