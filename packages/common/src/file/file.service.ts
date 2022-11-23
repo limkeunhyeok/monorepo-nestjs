4d9c6ec0625a7320e4349783b75513719068dfdb
limkeunhyeok <gorloom6425@naver.com>
2022-11-24 00:03:05 +0900

    feat: add read, download, create, delete file

--- packages/common/src/file/file.service.ts
+++ packages/common/src/file/file.service.ts
@@ -1,11 +1,42 @@
 import { Injectable } from "@nestjs/common";
-import { createReadStream } from "fs";
-import path from "path";
+import {
+  createReadStream,
+  existsSync,
+  readFileSync,
+  unlinkSync,
+  writeFileSync,
+} from "fs";
+import * as path from "path";
 
 @Injectable()
 export class FileService {
+  rootPath = path.join(__dirname, "../../");
+
   downloadFile(filePath) {
-    const stream = createReadStream(path.join(__dirname, filePath));
-    return stream;
+    const stream = createReadStream(path.join(this.rootPath, filePath));
+    return stream; // nest StreamableFile, options을 통해 type, dispositon, length 지정 가능
   }
+
+  // uploadFile() {}
+
+  readFile(filePath) {
+    const bufferedData = readFileSync(path.join(this.rootPath, filePath));
+    const jsonData = JSON.parse(bufferedData.toString());
+    return jsonData;
+  }
+
+  createFile(filePath, data) {
+    const stringData = JSON.stringify(data);
+    writeFileSync(path.join(this.rootPath, filePath), stringData);
+  }
+
+  // parsedFile() {}
+
+  deleteFile(filePath) {
+    if (existsSync(path.join(this.rootPath, filePath))) {
+      unlinkSync(path.join(this.rootPath, filePath));
+    }
+  }
+
+  // zippedFile() {}
 }
