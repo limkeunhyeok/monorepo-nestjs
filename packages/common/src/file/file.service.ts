f0a67c5658708593bd86b909e870783596c665f2
limkeunhyeok <gorloom6425@naver.com>
2022-11-25 23:05:32 +0900

    fix: file module

--- packages/common/src/file/file.service.ts
+++ packages/common/src/file/file.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { Response } from "express";
 import {
   createReadStream,
   existsSync,
   readFileSync,
+  ReadStream,
   unlinkSync,
   writeFileSync,
 } from "fs";
@@ -10,33 +12,35 @@ import * as path from "path";
 
 @Injectable()
 export class FileService {
-  rootPath = path.join(__dirname, "../../");
+  rootPath: string;
 
-  downloadFile(filePath) {
-    const stream = createReadStream(path.join(this.rootPath, filePath));
-    return stream; // nest StreamableFile, options을 통해 type, dispositon, length 지정 가능
+  constructor() {
+    this.rootPath = path.join(__dirname, "../../");
+  }
+
+  downloadFile(filepath, filename: string, res: Response) {
+    const stream = createReadStream(path.join(filepath, filename));
+    stream.pipe(res);
   }
 
   // uploadFile() {}
 
-  readFile(filePath) {
-    const bufferedData = readFileSync(path.join(this.rootPath, filePath));
+  readFile(filename: string): Record<string, unknown> {
+    const bufferedData = readFileSync(path.join(this.rootPath, filename));
     const jsonData = JSON.parse(bufferedData.toString());
     return jsonData;
   }
 
-  createFile(filePath, data) {
-    const stringData = JSON.stringify(data);
-    writeFileSync(path.join(this.rootPath, filePath), stringData);
-  }
-
   // parsedFile() {}
 
-  deleteFile(filePath) {
-    if (existsSync(path.join(this.rootPath, filePath))) {
-      unlinkSync(path.join(this.rootPath, filePath));
-    }
+  createFile(filename: string, data: Record<string, unknown>): void {
+    const stringData = JSON.stringify(data);
+    writeFileSync(path.join(this.rootPath, filename), stringData);
   }
 
-  // zippedFile() {}
+  deleteFile(filename: string): void {
+    if (existsSync(path.join(this.rootPath, filename))) {
+      unlinkSync(path.join(this.rootPath, filename));
+    }
+  }
 }
